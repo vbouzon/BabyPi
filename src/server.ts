676b287ab78f82a9fae0026c4dbba5fe634697b4
Vincent Bouzon <vincent.bouzon@live.fr>
2015-10-24 23:46:32 +0200

    clean

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path="server/camera_server.ts" />
+
+import http = require('http');
+import express = require('express');
+
+
+import srv = require('./server/camera_server');
+var app = express();
+
+//public website
+app.use(express.static(__dirname + '/public'));
+
+
+var server = http.createServer(app);
+var silence = new srv.Server();
+silence.initialize(server);
+
+server.listen(8080);
---
+++ src/server/camera_server.js
@@ -0,0 +1,62 @@
+/// <reference path="../typings/ws/ws.d.ts" />
+var WebSocketServer = require('ws');
+var child = require('child_process');
+var split = require('stream-split');
+var NALseparator = new Buffer([0, 0, 0, 1]); //NAL break
+var Server = (function () {
+    function Server() {
+        var _this = this;
+        this.options = {
+            width: 960,
+            height: 540,
+            fps: 12
+        };
+        this.initialize = function (server) {
+            _this.wss = new WebSocketServer.Server({ server: server });
+            _this.wss.on('connection', _this.new_client);
+        };
+        this.start_feed = function () {
+            var readStream = _this.get_feed();
+            _this.readStream = readStream;
+            readStream = readStream.pipe(new split.Splitter(NALseparator));
+            readStream.on("data", _this.broadcast);
+        };
+        this.get_feed = function () {
+            return child.spawn('raspivid', ['-t', '0', '-o', '-', '-w', _this.options.width.toString(), '-h', _this.options.height.toString(), '-fps', _this.options.fps.toString()], { stdio: ['ignore', 'pipe', 'inherit'] }).stdout;
+        };
+        this.broadcast = function (data) {
+            _this.wss.clients.forEach(function (socket) {
+                //if (socke)
+                //    return;
+                //socket.buzy = true;
+                //socket.buzy = false;
+                socket.send(Buffer.concat([NALseparator, data]), { binary: true }, function ack(error) {
+                    //socket.buzy = false;
+                });
+            });
+        };
+        this.new_client = function (socket) {
+            console.log('New guy');
+            socket.send(JSON.stringify({
+                action: "init",
+                width: _this.options.width,
+                height: _this.options.height,
+            }));
+            socket.on("message", function (data) {
+                var cmd = "" + data, action = data.split(' ')[0];
+                console.log("Incomming action '%s'", action);
+                if (action == "REQUESTSTREAM")
+                    this.start_feed();
+                if (action == "STOPSTREAM")
+                    this.readStream.pause();
+            });
+            socket.on('close', function () {
+                this.readStream.end();
+                console.log('stopping client interval');
+            });
+        };
+    }
+    return Server;
+})();
+exports.Server = Server;
+//# sourceMappingURL=camera_server.js.map
